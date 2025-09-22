66f4a955d005f312efd27433e684ab4ca360264f
emtsx <emichy@gmail.com>
2025-09-22 14:27:21 +0900

    add hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 print("Hello Python 🐍✨")
+=======
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+birthday = datetime. datetime(1985,6,11)
+print(birthday)
+>>>>>>> 6c7e070 (add hello.py)

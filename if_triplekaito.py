eb5672fd99d6fd381d6dd94f6cd2d5de3bd8cb6d
emtsx <emichy@gmail.com>
2025-09-22 14:13:11 +0900

    トリプルカイト判定プログラム追加

---
+++ if_triplekaito.py
@@ -0,0 +1,29 @@
+name = "Kaito"
+member_color = "orange"
+
+if name == "Kaito":
+    if member_color == "orange":
+        print("松倉海斗です")
+    elif member_color == "blue":
+        print("中村海人です")
+    elif member_color == "red":
+        print("宮近海斗です")
+    else:
+        print("あなたはトリプルカイトではありません")
+else:
+    print("あなたはトリプルカイトではありません")
+
+
+# 三項演算子：X if 条件 else Y
+
+# name = "Kaito"
+# member_color = "pink"
+#
+# result = (
+#     "松倉海斗です" if name == "Kaito" and member_color == "orange"
+#     else "中村海人です" if name == "Kaito" and member_color == "blue"
+#     else "宮近海斗です" if name == "Kaito" and member_color == "red"
+#     else "あなたはトリプルカイトではありません"
+# )
+#
+# print(res
